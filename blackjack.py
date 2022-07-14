7f4cb10d121ccfdec5444009a79c37151b88c095
akherbst2 <alyssaherbst@fb.com>
2022-07-13 18:32:03 -0700

    Percentage start $

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,5 @@
 import argparse
-
+import random
 
 def run_simu(
     prob_win,
@@ -8,7 +8,26 @@ def run_simu(
     bet_unit,
     pocket_unit,
     ):
-    return start_money
+
+    table_money = start_money
+    pocket_money = 0
+
+    assert prob_win + prob_push < 1.0
+    
+    bet_amount = bet_unit
+    while(table_money - bet_amount > 0):
+        game_result = random.random()
+        if(game_result < prob_win):
+            table_money += bet_amount
+            table_money -= pocket_unit
+            pocket_money += pocket_unit
+            bet_amount = bet_unit
+        elif(game_result < prob_win + prob_push):
+            pass # TODO
+        else: # Lose game
+            table_money -= bet_amount
+            bet_amount *= 2 # double your bet    
+    return pocket_money + table_money
 
 def main(
         trials,
@@ -22,23 +41,25 @@ def main(
     for i in range(trials):
         sum_money_trials += run_simu(prob_win, prob_push, start_money, bet_unit, pocket_unit)
 
-    avg_win = sum_money_trials / trials
+    avg_win = sum_money_trials * 1.0 / trials
+    perc_start = avg_win / start_money
     print(
     """
     Num Trials:\t{trials}
     Avg win:\t{avg_win}
+    % Start $:\t{perc_start}%
     """
-        .format(trials=trials, avg_win=avg_win)
+        .format(trials=trials, avg_win=avg_win, perc_start=perc_start * 100)
     )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--trials', dest='trials', action='store_const', const='trials', default=10)
-    parser.add_argument('--prob-win', dest='prob_win', action='store_const', const='prob_win', default=0.45) 
-    parser.add_argument('--prob-push', dest='prob_push', action='store_const', const='prob_push', default=0.0) 
-    parser.add_argument('--start-money', dest='start_money', action='store_const', const='start_money', default=8)
-    parser.add_argument('--bet-unit', dest='bet_unit', action='store_const', const='bet_unit', default=1) 
-    parser.add_argument('--pocket-unit', dest='pocket_unit', action='store_const', const='pocket_unit', default=1)
+    parser.add_argument('-t', '--trials', type=int,  default=10)
+    parser.add_argument('-w', '--prob-win', type=float, default=0.425) 
+    parser.add_argument('-p', '--prob-push', type=float, default=0.08) 
+    parser.add_argument('-s', '--start-money', type=float, default=8.0)
+    parser.add_argument('-b', '--bet-unit', type=float, default=1.0) 
+    parser.add_argument('-o', '--pocket-unit', type=float, default=1.0)
     args = parser.parse_args()
     main(args.trials, 
          args.prob_win,
